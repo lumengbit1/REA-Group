825238e307054ebabd7c0838a49afff6baa7e8a6
Meng Lu <meng@burnsred.com.au>
2021-06-26 12:50:45 +1000

    name changed

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import Enzyme, { shallow } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-import App from '../App'
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import App from '../App';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe(`Test <App />`, () => {
+describe('Test <App />', () => {
   const warpper = shallow(<App />);
-  it('1. Include one <div>',()=>{
-      expect(warpper.find('div').length).toBe(1);
-  })
+  it('1. Include one <div>', () => {
+    expect(warpper.find('div').length).toBe(1);
+  });
 
   it('2. "Header" Could be rendered', () => {
-      expect(warpper.find('Content').exists());
-  })
-})
+    expect(warpper.find('Content').exists());
+  });
+});
