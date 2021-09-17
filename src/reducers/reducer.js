0a52ffc5e69c5333aba5d3770e7f61c23157d335
Administrator <lumengbit1@gmail.com>
2021-09-17 13:46:26 +1000

    remove immutable from reducer

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,6 +1,6 @@
 import { handleActions } from 'redux-actions';
 import { fromJS } from 'immutable';
-import { produce } from 'immer';
+import { produce, current } from 'immer';
 import {
   get_results_successed,
   get_saved_successed,
@@ -27,50 +27,25 @@ const reducer = handleActions(
   {
     [get_results]: (state) => state,
     [get_saved]: (state) => state,
-    [saved_loading]: (state, action) => {
-      const nextState = produce(state, draft => {
-        draft.loading.saved = action.payload;
-      });
-      return nextState;
-    },
-    [results_loading]: (state, action) => {
-      const nextState = produce(state, draft => {
-        draft.loading.results = action.payload;
-      });
-      return nextState;
-    },
-    [get_results_successed]: (state, action) => {
-      const nextState = produce(state, draft => {
-        draft.results = action.payload.data;
-      });
-      return nextState;
-    },
-    [get_saved_successed]: (state, action) => {
-      const nextState = produce(state, draft => {
-        draft.saved = action.payload.data;
-      });
-      return nextState;
-    },
-    [get_failed]: (state, action) => {
-      const nextState = produce(state, draft => {
-        draft.errors = action.payload.data;
-      });
-      return nextState;
-    },
-    [add_property]: (state, action) => {
-      const selectedData = state.get('results').find((item) => item.get('id') === action.payload);
+    [saved_loading]: (state, action) => produce(state, draft => {draft.loading.saved = action.payload}),
+    [results_loading]: (state, action) => produce(state, draft => {draft.loading.results = action.payload}),
+    [get_results_successed]: (state, action) => produce(state, draft => {draft.results = action.payload.data}),
+    [get_saved_successed]: (state, action) => produce(state, draft => {draft.saved = action.payload.data}),
+    [get_failed]: (state, action) => produce(state, draft => {draft.errors = action.payload.data}),
+    [add_property]: (state, action) => produce(state, draft => {
+      const selectedData = draft.results.find((item) => item.id === action.payload);
 
-      if (state.get('saved').find((item) => item.get('id') === selectedData.get('id'))) {
-        return state;
+      if (draft.saved.find((item) => item.id === selectedData.id)) {
+        return;
       }
 
-      return state.update('saved', (val) => val.push(selectedData));
-    },
-    [remove_property]: (state, action) => {
-      const selectedData = state.get('saved').find((item) => item.get('id') === action.payload);
+      draft.saved.push(selectedData);
+    }),
+    [remove_property]: (state, action) => produce(state, draft => {
+      const selectedData = draft.saved.find((item) => item.id === action.payload);
 
-      return state.update('saved', (item) => item.filter((val) => val.get('id') !== selectedData.get('id')));
-    },
+      draft.saved = draft.saved.filter((val) => val.id !== selectedData.id);
+    }),
   },
   initialState,
 );
